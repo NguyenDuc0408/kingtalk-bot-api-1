e1b81293a578f0676d4746a7470c40eb895fd932
trtung901 <tungtran901@gmail.com>
2021-03-18 15:44:32 +0700

    private init

--- configs.go
+++ configs.go
@@ -10,8 +10,8 @@ import (
 // KingTalk constants
 const (
 	// *NOTE: This is testing endpoint, not official.
-	APIEndpoint  = "http://103.109.41.25:8080/bot%s/%s"
-	FileEndpoint = "http://103.109.41.25:8080/file/bot%s/%s"
+	APIEndpoint  = "http://172.26.37.36:8081/bot%s/%s"
+	FileEndpoint = "http://172.26.37.36:8081/file/bot%s/%s"
 )
 
 // Constant values for ChatActions
